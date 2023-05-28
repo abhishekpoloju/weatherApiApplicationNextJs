452aa27c374b6bfa1ee31adcead597fcfab7ed4c
abhishek.poloju <abhishek.poloju@gmail.com>
2023-05-28 08:32:17 +0530

    started weather api

---
+++ components/NavBar.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import React, { ChangeEvent, SetStateAction, useState } from 'react'
+import Image from 'next/image'
+
+
+
+export const NavBar = () => {
+  const [input,setInput]=useState<string>("")
+  const inputChange=(e:ChangeEvent<HTMLInputElement>)=>{
+    setInput(e.target.value)
+  }
+  const searchBarFormSubmission=(e:ChangeEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    const value:string=(e.currentTarget.elements.item(0)?.getAttribute("value")!)
+    if(value){
+      
+    }
+  }
+  return (
+    <nav className='flex h-20 bg-slate-400 w-full px-5 items-center justify-between
+    '>
+      <Link href={`/`}><span className='text-white text-3xl font-extrabold'>WeatherApplication</span></Link>
+      <form className='relative' onSubmit={searchBarFormSubmission}>
+      <input value={input} id='searchBar' onChange={inputChange} className='rounded-md border border-black'/>
+      <button type='submit'><Image src={"icons8-search.svg"} alt='' width={20} height={20} className='absolute top-[1.5px] right-2'/></button>
+      </form>
+    </nav>
+  )
+}
+
+
+
+
+
---
+++ pages/random.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const random = () => {
+  return (
+    <div className='h-screen w-full flex items-center justify-center'>
+      hiii
+    </div>
+  )
+}
+
+export default random
