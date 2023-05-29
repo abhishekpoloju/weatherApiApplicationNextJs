7cfa6b1bc4b19b457c750546afced21f141f41ba
abhishek.poloju <abhishek.poloju@gmail.com>
2023-05-29 07:27:36 +0530

    some uI changes

--- components/ApiResultCard.tsx
+++ components/ApiResultCard.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import { apiFormat } from '@/pages/interfaces/interfaces'
-export const ApiResultCard = (props:apiFormat) => {
+import React from "react";
+import { apiFormat } from "@/pages/interfaces/interfaces";
+export const ApiResultCard = (props: apiFormat) => {
   return (
-    <div className='flex w-[650px] bg-slate-500'>
-        
+    <div className="w-[650px] bg-slate-500">
+      <div className="flex w-full p-2">
+        <div className="flex gap-2">
+          <span className="font-medium">
+            country: <span className="font-semibold">{props.sys.country}</span>
+          </span>
+          <span className="font-medium">
+            country: <span className="font-semibold">{props.sys.country}</span>
+          </span>
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <div className="flex flex-col items-center justify-center">
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
